495bef2dc7b5103e662872bb73534704a038a866
Vishal Venkatraman <ihandsomeguy97@gmail.com>
2019-10-24 13:03:30 +0800

    Removal of Boilerplate + New Packages

---
+++ src/SideBar.jsx
@@ -0,0 +1,15 @@
+import React, { useState, Fragment } from 'react';
+
+import {Button, Drawer, List, ListItem} from '@material-ui/core';
+
+function Sidebar() {
+    return (
+        <Fragment>
+            <Button>
+                May the Lord have mercy on us
+            </Button>
+        </Fragment>
+    );
+}
+
+export default Sidebar;
