8b3249053c39c5e652120277e2ff9dc90e4d5a8d
rhea1310 <guptarhea13@gmail.com>
2019-11-06 12:57:13 +0800

    Foreign keys relations added

--- backend/system/models.py
+++ backend/system/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class PBI(models.Model):
-    pbi_id = models.IntegerField(primary_key=True)
-    project_id = models.IntegerField()
-    story_pts = models.IntegerField()
-    name = models.CharField(max_length=30)
-    desc = models.CharField(max_length=100)
-
-
 class Developer(models.Model):
     e_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
@@ -18,16 +10,23 @@ class Manager(models.Model):
 
 class Project(models.Model):
     p_id = models.IntegerField(primary_key=True)
-    owner_id = models.IntegerField()
-    manager_id = models.IntegerField()
+    owner_id = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    manager_id = models.ForeignKey(Manager, on_delete=models.CASCADE)
     date_created = models.DateTimeField()
     last_modified = models.DateTimeField()
     name = models.CharField(max_length=30)
     desc = models.CharField(max_length=100)
 
+class PBI(models.Model):
+    pbi_id = models.IntegerField(primary_key=True)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    story_pts = models.IntegerField()
+    name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=100)
+
 class Task(models.Model):
     task_id = models.IntegerField(primary_key=True)
-    pbi_id = models.IntegerField()
+    pbi_id = models.ForeignKey(PBI,on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     desc = models.CharField(max_length=100)
     status = models.IntegerField()
