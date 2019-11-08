a40d4d09dcfcf65810e84281c7451e39c958c757
Adya <adyasingh@Adyas-Mac.local>
2019-11-08 20:44:50 +0800

    Project Routing

--- backend/frontend/src/components/NewProject.js
+++ backend/frontend/src/components/NewProject.js
@@ -5,7 +5,6 @@ import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-
 import { Link } from 'react-router-dom';
 
 
