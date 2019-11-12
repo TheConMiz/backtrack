a09a5273a4fac3c73f2124f9cba8f743e10bd2fd
Vishal Venkatraman <ihandsomeguy97@gmail.com>
2019-11-12 14:55:25 +0800

    Models.py - Update

--- backend/system/models.py
+++ backend/system/models.py
@@ -1,50 +1,72 @@
 from django.db import models
 
+# Class for Developer
 class Developer(models.Model):
-    e_id = models.IntegerField(primary_key=True)
+    id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=30)
 
+# Class for Manager
 class Manager(models.Model):
-    e_id = models.IntegerField(primary_key=True)
+    id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=30)
 
 class Project(models.Model):
-    p_id = models.AutoField(auto_created = True, primary_key = True)
+    id = models.AutoField(auto_created=True, primary_key=True)
     owner_id = models.ForeignKey(Developer, on_delete=models.CASCADE)
     manager_id = models.ForeignKey(Manager, on_delete=models.CASCADE)
-    date_created = models.DateTimeField()
-    last_modified = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=30)
     desc = models.CharField(max_length=100)
+
 
 class PBI(models.Model):
-    pbi_id = models.AutoField(auto_created=True, primary_key=True)
+    id = models.AutoField(auto_created=True, primary_key=True)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
-    story_pts = models.IntegerField()
+    story_pts = models.IntegerField(default=0)
     name = models.CharField(max_length=30)
     desc = models.CharField(max_length=100)
-    priority= models.IntegerField()
+
+    # Status Key
+    ## 1 - To-Do
+    ## 2 - In Progress
+    ## 3 - Under Review
+    ## 4 - Completed
+    status = models.IntegerField(default=1)
+
+    priority = models.IntegerField(default=0)
+
 
 class Task(models.Model):
-    task_id = models.IntegerField(primary_key=True)
-    pbi_id = models.ForeignKey(PBI,on_delete=models.CASCADE)
+    id = models.AutoField(auto_created=True, primary_key=True)
+    pbi_id = models.ForeignKey(PBI, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     desc = models.CharField(max_length=100)
-    status = models.IntegerField()
 
-    # ADDED FIELD TO DEAL WITH SPRINT BOARD
-    priority = models.IntegerField(default=task_id)
-    
-class Working(models.Model):
-    task_id= models.ForeignKey(Task,on_delete=models.CASCADE)
+    # Status Key
+    ## 1 - To-Do
+    ## 2 - In Progress
+    ## 3 - Under Review
+    # ## 4 - Completed
+    status = models.IntegerField(default=1)
+
+    priority = models.IntegerField(default=0)
+
+
+class TaskOwnership(models.Model):
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     dev_id = models.ForeignKey(Developer, on_delete=models.CASCADE)
-    
 
-# class Sprint(models.Model):
-#     sprint_id = models.IntegerField()
-#     pbi_id = models.ForeignKey(PBI, on_delete=models.CASCADE)
-#     task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
-#     tsp = models.IntegerField()
-#     #tsp=total story points
-#     csp = models.IntegerField()
-#     #completed story points
+    # Unit of effort: hours
+    effort = models.IntegerField(default=0)
+
+
+class Sprint(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    total_story_points = models.IntegerField(default=0)
+    completed_story_points = models.IntegerField(default=0)
+
+
+class PBIsInSprint(models.Model):
+    sprint_id = models.ForeignKey(Sprint, on_delete=models.CASCADE)
+    pbi_id = models.ForeignKey(PBI, on_delete=models.CASCADE)
